44aa8b87b0005c1518a07dd4fbf2afc293d76db0
fukata <tatsuya.fukata@gmail.com>
2018-10-24 22:18:23 +0700

    add thailand plugin

---
+++ mybot/plugins/thailand.py
@@ -0,0 +1,28 @@
+# coding: utf-8
+
+from slackbot.bot import respond_to
+import http.client
+from urllib.parse import urlencode, quote_plus
+import json
+from datetime import tzinfo,timedelta,datetime,timezone
+import os
+
+MASTER_DAYS = [
+            {"date": "2018-03-01"},
+            {"date": "2018-05-29"},
+            {"date": "2018-07-27"},
+            {"date": "2018-07-28"},
+            {"date": "2018-10-24"},
+        ]
+
+def get_no_alcohol_day():
+    message = ""
+    for day in MASTER_DAYS:
+        message += "- {0}\n".format(day["date"])
+    return message
+
+@respond_to('thailand:no-alcohol-day$')
+@respond_to('thailand:no-alcohol-day (.*)')
+def no_alcohol_day(message, keyword=None):
+    notify_message = get_no_alcohol_day()
+    message.reply(notify_message)
